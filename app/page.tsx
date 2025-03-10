0dc1b17a840b091e80655879ba67147e65b83370
lijnati3 <lijnati3@gmail.com>
2025-03-10 06:41:14 +0000

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -137,8 +137,8 @@ export default function Personal() {
       >
         <div className="flex-1">
           <p className="text-zinc-600 dark:text-zinc-400">
-            Focused on creating intuitive and performant web experiences.
-            Bridging the gap between design and development.
+          Full Stack Developer with 3+ years of hands-on experience designing, developing, and implementing applications and solutions using a range of technologies and programming languages. 
+          Seeking to leverage broad development experience and hands-on technical expertise in challenging role as a Full Stack Developer.
           </p>
         </div>
       </motion.section>
